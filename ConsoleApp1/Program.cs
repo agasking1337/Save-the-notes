03cfb5c7f3b61fa1dd1c6470ae0418caa3458558
Rubin.Muresan <Rubin.Muresan@ACCESS-56H53T3>
2024-07-10 10:11:55 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+
+namespace SaveData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a new file path where to save the data.
+            string filePath = "saved_data.txt";
+
+
+            //checking if there is an existing file if not to create one.
+            if (!File.Exists(filePath)) 
+            {
+                Console.WriteLine("Generating a new file named " + filePath);
+            }
+            else
+            {
+                Console.WriteLine("Adding text to the existing file " + filePath);
+            }
+
+            //Appending new text to the file
+            using (StreamWriter userInput = File.AppendText(filePath))
+            {
+                Console.WriteLine("Add your notes. ");
+                userInput.WriteLine(Console.ReadLine());
+            }
+
+            Console.WriteLine("Text saved to " + filePath);
+
+            //reading all the saved text 
+            string savedData = File.ReadAllText(filePath);
+            Console.WriteLine("Your previous notes are: ");
+            Console.WriteLine(savedData);
+            
+        }
+    }
+}
